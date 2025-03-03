ad8d8ac403d59c65e0fa669e28c1acb8d8d8f15a
Sheel2007 <sheel_arsenal@icloud.com>
2025-03-03 08:57:23 -0500

    Fixed Voltage and Added StepWise Function

--- dac-converter.py
+++ dac-converter.py
@@ -18,27 +18,48 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create MCP4725 instance
 dac = MCP4725(i2c, address=0x60)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(TRANSISTOR_PIN, GPIO.OUT)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(TRANSISTOR_PIN, GPIO.OUT)
 
-GPIO.output(TRANSISTOR_PIN, GPIO.LOW)
+# GPIO.output(TRANSISTOR_PIN, GPIO.LOW)
 # transistor.on()
 
 # MCP4725 is a 12-bit DAC, so values range from 0 (0V) to 4095 (Vcc)
-# Calculate the appropriate value for 2.5V output
+start_voltage = 0
 output_voltage = 1.5  # Desired output voltage
 vcc = 3.3  # DAC reference voltage
 dac_value = int((output_voltage / vcc) * 4095)
 
 # Set DAC output
+
+current_voltage = start_voltage
+
+dac.raw_value = start_voltage
+
+step = 0.1
+print(f'Starting on {current_voltage}V')
+
+while True:
+    time.sleep(1)
+    current_voltage += step
+    dac.raw_value = int((current_voltage / vcc) * 4095)
+    print(f'Changed to {current_voltage}V')
+    if current_voltage >= output_voltage:
+        current_voltage = output_voltage
+        dac.raw_value = dac_value
+        print(f'Now ending on {current_voltage}')
+        break
+
+
+# time.sleep(10)
+
 # dac.raw_value = dac_value
 
-time.sleep(2)
-
 # transistor.off()
-GPIO.output(TRANSISTOR_PIN, GPIO.HIGH)
+# GPIO.output(TRANSISTOR_PIN, GPIO.HIGH)
 
 # GPIO.cleanup()
+# i2c.deinit()
 # exit()
 
 print(f"DAC output set to {output_voltage}V")
@@ -47,10 +68,9 @@ print(f"DAC output set to {output_voltage}V")
 test = input()
 
 if test:
-    # output_voltage = 0
-    # dac.raw_value = int((output_voltage / vcc) * 4095)
+    dac.raw_value = start_voltage
     i2c.deinit()
     # transistor.on()
-    GPIO.output(TRANSISTOR_PIN, GPIO.LOW)
+    # GPIO.output(TRANSISTOR_PIN, GPIO.LOW)
     print("Program terminated.")
 
